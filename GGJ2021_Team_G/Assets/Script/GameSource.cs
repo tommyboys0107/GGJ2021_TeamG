93b3a4954f086c11aa9bc8998ad1c30bb0c6a23b
wayen22884 <65549023+wayen22884@users.noreply.github.com>
2021-01-31 09:42:29 +0800

    Music 音樂功能

--- GGJ2021_Team_G/Assets/Script/GameSource.cs
+++ GGJ2021_Team_G/Assets/Script/GameSource.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GameSource : MonoBehaviour
 {
     public GameObject ItemGlasses;
     public GameObject ItemColorWorld;
-    public AudioSource CameraSource;
+    public AudioSource MusicSource;
+    public AudioClip MusicClip;
 }
