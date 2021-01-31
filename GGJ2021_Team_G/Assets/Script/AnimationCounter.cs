7a3dff357e737e0eebd5451717f11661bb200aab
l4dnigel <l4dnigel@gmail.com>
2021-01-31 13:35:04 +0800

    調整 Boss 動作檔，三段動作輪播

---
+++ GGJ2021_Team_G/Assets/Script/AnimationCounter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCounter : MonoBehaviour
+{
+    public BossUnit boss;
+
+    public void Counter()
+    {
+        if (boss != null)
+        {
+            boss.AnimationPlayCount();
+        }
+    }
+}
